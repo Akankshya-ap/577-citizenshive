72f4e948df65ce6b39fd31f025df70e71960afe9
Akankshya Patra <ayesha.patra2@gmail.com>
2020-11-05 17:53:17 -0800

    profile images

--- src/app1/views.py
+++ src/app1/views.py
@@ -230,6 +230,51 @@ def dashboard_view(request, *args, **kwargs) :
     else :
         return redirect('caregiver_dashboard_view')
 
+# def caregiver_dashboard_view(request, *args, **kwargs) :
+#     context = {}
+#     context['user_type'] = request.session['user_type']
+#     if 'email' in request.session :
+#         email = request.session['email']
+
+#     if request.method == 'POST' :
+#         # name = request.session['name']
+#         # email = request.session['email']
+#         dob = request.POST['dob']
+#         availability = request.POST['availability']
+#         zip_code = request.POST['zip']
+#         city = request.POST['city']
+#         state = request.POST['state']
+#         start_date = request.POST['start_date']
+#         end_date = request.POST['end_date']
+#         bio = request.POST['bio']
+#         profile_image = request.FILES['profile_image']
+
+#         record = Caregiver.objects.get(email=email)
+#         record.name = record.name 
+#         record.availability = availability
+#         record.zip_code = zip_code
+#         record.city = city
+#         record.state = state
+#         record.start_date = start_date
+#         record.end_date = end_date
+#         record.bio = bio
+#         record.dob = dob if dob!="" else None
+#         # try:
+#         record.profile_image = profile_image
+#         # except:
+#         #     x=1
+
+#         record.save()
+#         context['record'] = record
+#         context['profile_image_url'] = record.profile_image.url
+#         context['image_object'] = record.profile_image
+#         return render(request, 'caregiver_dashboard.html', context)
+#     else :
+#         context['record'] = Caregiver.objects.get(email = request.session['email'])
+#         context['profile_image_url'] = 'default'
+#         context['image_object'] = record.profile_image
+#         return render(request, 'caregiver_dashboard.html', context)
+
 def caregiver_dashboard_view(request, *args, **kwargs) :
     context = {}
     context['user_type'] = request.session['user_type']
@@ -244,10 +289,8 @@ def caregiver_dashboard_view(request, *args, **kwargs) :
         zip_code = request.POST['zip']
         city = request.POST['city']
         state = request.POST['state']
-        start_date = request.POST['start_date']
-        end_date = request.POST['end_date']
         bio = request.POST['bio']
-        # profile_image = request.FILES['profile_image']
+        profile_image = request.FILES['profile_image']
 
         record = Caregiver.objects.get(email=email)
         record.name = record.name 
@@ -255,24 +298,20 @@ def caregiver_dashboard_view(request, *args, **kwargs) :
         record.zip_code = zip_code
         record.city = city
         record.state = state
-        record.start_date = start_date
-        record.end_date = end_date
         record.bio = bio
         record.dob = dob if dob!="" else None
-        # record.profile_image = profile_image
+        record.profile_image = profile_image
 
         record.save()
         context['record'] = record
-        #context['profile_image_url'] = record.profile_image.url
-        #context['image_object'] = record.profile_image
+        context['profile_image_url'] = record.profile_image.url
+        context['image_object'] = record.profile_image
         return render(request, 'caregiver_dashboard.html', context)
     else :
         context['record'] = Caregiver.objects.get(email = request.session['email'])
-        #context['profile_image_url'] = 'default'
-        #context['image_object'] = record.profile_image
+        # context['profile_image_url'] = record.profile_image.url
+        # context['image_object'] = record.profile_image
         return render(request, 'caregiver_dashboard.html', context)
-
-
 
 def senior_dashboard_view(request, *args, **kwargs) :
     context = {}
@@ -289,9 +328,7 @@ def senior_dashboard_view(request, *args, **kwargs) :
         city = request.POST['city']
         state = request.POST['state']
         bio = request.POST['bio']
-        start_date = request.POST['start_date']
-        end_date = request.POST['end_date']
-        # profile_image = request.FILES['profile_image']
+        profile_image = request.FILES['profile_image']
 
         record = Senior.objects.get(email=email)
         record.name = record.name 
@@ -299,21 +336,19 @@ def senior_dashboard_view(request, *args, **kwargs) :
         record.zip_code = zip_code
         record.city = city
         record.state = state
-        record.start_date = start_date
-        record.end_date = end_date
         record.bio = bio
         record.dob = dob if dob!="" else None
-        # record.profile_image = profile_image
+        record.profile_image = profile_image
 
         record.save()
         context['record'] = record
-        #context['profile_image_url'] = record.profile_image.url
-        #context['image_object'] = record.profile_image
+        context['profile_image_url'] = record.profile_image.url
+        context['image_object'] = record.profile_image
         return render(request, 'senior_dashboard.html', context)
     else :
         context['record'] = Senior.objects.get(email = request.session['email'])
-        #context['profile_image_url'] = 'default'
-        #context['image_object'] = record.profile_image
+        # context['profile_image_url'] = 'default'
+        # context['image_object'] = record.profile_image
         return render(request, 'senior_dashboard.html', context)
 
 
